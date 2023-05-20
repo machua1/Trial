a59d03379c6b1b14c25d7d8a8713c846254d86fc
Evans Mwaura Machua <mwauramachua@gmail.com>
2023-05-20 10:55:21 +0300

    trial.py

---
+++ trial.py
@@ -0,0 +1,14 @@
+
+print("Hello world!")
+
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+
+action= ['run', 'fight', 'hide']
+action.append ('cry')
+action[2]= ('sing')
+print('Hello World')
+print(action)
+name="Evans "+"Mwaura"
+print(name)
